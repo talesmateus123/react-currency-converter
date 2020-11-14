e1018cf7be35a8fa0fe243710525225f4c38b3b4
Tales Ferreira <talesmateus1999@hotmail.com>
2020-11-14 15:59:50 -0400

    The main component and its tests have been implemented; Trash files have been removed.

---
+++ src/CurrencyConverter/CurrencyConverter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CurrencyConverter.css';
+
+function CurrencyConverter() {
+  return (
+    <div>
+      <h1>Currency Converter</h1>
+    </div>
+  );
+}
+
+export default CurrencyConverter;
--- src/CurrencyConverter/CurrencyConverter.test.js
+++ src/CurrencyConverter/CurrencyConverter.test.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM, { unmountComponentAtNode } from 'react-dom'
-import App from './App'
+import CurrencyConverter from './CurrencyConverter'
 import '@testing-library/jest-dom/extend-expect'
 
-describe('App', () => {
+describe('CurrencyConverter', () => {
     it('renders without crash', () => {
         const div = document.createElement('div')
-        ReactDOM.render(<App />, div);
+        ReactDOM.render(<CurrencyConverter />, div);
         unmountComponentAtNode(div)
     });
 })
