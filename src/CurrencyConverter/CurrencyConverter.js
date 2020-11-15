cb873854181f11fd32fafff241f3c7b8c8c8aaef
Tales Ferreira <talesmateus1999@hotmail.com>
2020-11-15 01:18:42 -0400

    Implemented request to fixer io

--- src/CurrencyConverter/CurrencyConverter.js
+++ src/CurrencyConverter/CurrencyConverter.js
@@ -11,10 +11,13 @@ import {
 } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
 
 import ListCurrencies from './ListCurrencies/ListCurrencies'
 
 function CurrencyConverter() {
+
+  const FIXER_URL = 'http://data.fixer.io/api/latest?access_key=dbfaac1825886a94b161a597d505a6b7'
 
   const [value, setValue] = useState('1')
   const [fromCurrency, setFromCurrency] = useState('BRL')
@@ -23,23 +26,47 @@ function CurrencyConverter() {
   const [validatedForm, setValidatedForm] = useState(false)
   const [showModal, setShowModal] = useState(false)
   const [resultConversion, setResultConversion] = useState('')
+  const [errorMsg, setErrorMsg] = useState(null)
 
   const handleCloseModal = event => {
-    setValue('1')
-    setFromCurrency('BRL')
-    setToCurrency('USD')
     setValidatedForm(false)
     setShowModal(false)
   }
 
-  const toConvert = event => {
+  const toConvert = async event => {
     event.preventDefault()
     setValidatedForm(true)
     if(event.currentTarget.checkValidity() === true) {
-      // TODO Implements call to fixer.io
-      setShowModal(true)
-
+      setShowSpinner(true)
+      axios.get(FIXER_URL)
+      .then(res => {
+        const quotation = getQuotation(res.data)
+        setShowSpinner(false)
+        if(quotation) {
+          setResultConversion(`${value} ${fromCurrency} = ${quotation} ${toCurrency}`)
+          setShowModal(true)
+          setErrorMsg(false)
+        }
+        else {
+          setErrorMsg('Cannot get quotation value')
+        }
+      })
+      .catch(err => {
+        setShowSpinner(false)
+        setErrorMsg(err)
+      })
     }
+  }
+
+  const getQuotation = quotationData => {
+    if(!quotationData || quotationData.success !== true) {
+      return false
+    }
+    const fromQuotation = quotationData.rates[fromCurrency]
+    const toQuotation = quotationData.rates[toCurrency]
+
+    const quotation = (1 / fromQuotation * toQuotation) * value
+    return quotation.toFixed(2)
   }
 
   return (
@@ -47,9 +74,9 @@ function CurrencyConverter() {
       <Jumbotron>
         <Alert 
           variant="danger"
-          show={true}
+          show={errorMsg}
         >
-          Error when converting, please try again later.
+          Error when converting: {errorMsg}. Please try again later.
         </Alert>
         <Modal show={showModal} onHide={handleCloseModal}>
           <Modal.Header closeButton>
